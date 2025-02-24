b7dd752b5aac1b48360dee1f9cdef87aa7ac8223
Manuel Domínguez <mddmanuel8@gmail.com>
2025-02-24 14:11:57 +0100

    basico node

--- 01_Basico_node_mongodb/controlador/cocheController.js
+++ 01_Basico_node_mongodb/controlador/cocheController.js
@@ -1,13 +1,101 @@
 const express = require("express")
 const ruta = express.Router()
 const Coche = require("../modelo/Coche")
+const coches = require("../bbdd/coches")
 
-const findAll = async (req, res) => {
+const eliminar = async (req, res) => {
+    try{
+         
+         result = await Coche.findByIdAndDelete(req.params.id)
 
-    cocheList = await Coche.find()
-    return res.status(200).send(cocheList)
+         if(!result)
+            return res.status(404).send("Coche no existe")
+
+         return   res.status(200).send("Coche eliminado");
+
+     }catch(err){
+         return res.status(500).json({mensaje: "Coche no se ha podido eliminar", err})
+ }
+ }
+const modificar = async (req, res) => {
+    try{
+         const coche = req.body;
+         result = await Coche.findByIdAndUpdate(req.params.id, coche, {new:true, runValidators: true})
+
+         if(!result)
+            return res.status(404).send("Coche no existe")
+
+
+         return   res.status(200).send(result);
+     }catch(err){
+         return res.status(500).json({mensaje: "error interno", err})
+ }
+ }
+
+
+const alta = async (req, res) => {
+   try{
+        const coche = req.body;
+        cocheNuevo = new Coche(coche);
+        result = await cocheNuevo.save();
+        res.status(200).send(result);
+    }catch(err){
+        return res.status(500).json({mensaje: "error interno", err})
+}
 }
 
+
+const findByPrecioMayor = async (req, res) => {
+    const precioBuscado = req.params.precio
+    const coches = await Coche.find({precio: {$gt:precioBuscado}})
+    return res.status(200).send(coches);
+}
+
+const findByMarca = async (req, res) => {
+    const coches = await Coche.find({marca: req.params.marca})
+    return res.status(200).send(coches);
+}
+const findAll = async (req, res) => {
+    result = await Coche.find()
+
+    return res.status(200).send(result);
+   
+
+}
+
+const findById = async (req, res) => {
+  try{
+    const coche = await Coche.findById(req.params.id)
+
+    if (!coche)
+        return res.status(404).send("Coche no existe")
+
+
+    return res.status(200).send(coche);
+}catch(err){
+    return res.status(500).json({mensaje: "error interno", err})
+}
+
+}
+
+const cargaInicial = async (req, res) => {
+
+    await Coche.insertMany(coches)
+    return res.status(200).send("Carga finalizada");
+
+}
+
+
+
+
+
 module.exports = {
-    findAll
+    findAll,
+    cargaInicial,
+    findById,
+    findByMarca,
+    findByPrecioMayor,
+    alta,
+    modificar,
+    eliminar
 }
--- 01_Basico_node_mongodb/index.js
+++ 01_Basico_node_mongodb/index.js
@@ -1,18 +1,20 @@
-const express = require("express");
+const express = require("express")
 const app = express();
+const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const rutaCoche = require("./rutas/cocheRuta")
+const rutaUsuario = require("./rutas/usuarioRuta")
 
-const rutaCoches = require("./rutas/cocheRuta")
 
 //middleware
-app.use(express.json());
-app.use("/api/coches", rutaCoches)
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use("/api/coches", rutaCoche)
+app.use("/api/admon/usuarios", rutaUsuario)
 
-//conexion a mongoose
+// conexion a mongoose
 mongoose.connect("mongodb://127.0.0.1:27017/bbdd-coches_2025")
-.then(() => console.log("Base de datos conectada"))
-.catch(() => console.log("Base de datos NO conectada"))
+.then(()=> console.log("Base de datos mongo conectada"))
+.catch(() => console.log("Base de datos NOOO conectada"))
 
-
-
-app.listen(3000, () => console.log("Arrancando por el puerto 3000"));
+app.listen(3000, ()=> console.log("arrancando por el puerto 3000"))
